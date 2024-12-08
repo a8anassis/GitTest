f02db61f02bc531f2d5b7db3b51d19951751307c
Th. Androutsos <a8anassis@gmail.com>
2024-12-08 22:03:18 +0200

    Calulator added

---
+++ src/gr/aueb/cf/calc/Calculator.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.calc;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int div(int a, int b) {
+        return a / b;
+    }
+}
