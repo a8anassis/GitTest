6e78c3744fc509b9e6dba4ec09ad543d08e69ea2
Th. Androutsos <a8anassis@gmail.com>
2024-12-08 22:18:19 +0200

    sub created

--- src/gr/aueb/cf/calc/Calculator.java
+++ src/gr/aueb/cf/calc/Calculator.java
@@ -10,7 +10,13 @@ public class Calculator {
         return a + b;
     }
 
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
     public static int div(int a, int b) {
         return a / b;
     }
+
+
 }
